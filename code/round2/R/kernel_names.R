7998fa8fc7b900c366822a491354922d492d5757
mostafakarimi71 <mostafakarimi71@yahoo.com>
2015-12-12 19:06:07 -0600

    add all of kernel train and test of all combinations of cell line kernel and drug kernel

--- code/round2/R/kernel_names.R
+++ code/round2/R/kernel_names.R
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env Rscript
 mutations_angular_similarity <- c('../../data/round2/kernels/angular_similarity_mutations_driver',
                                   '../../data/round2/kernels/angular_similarity_mutations_nonsilent',
                                   '../../data/round2/kernels/angular_similarity_mutations_original')
@@ -11,9 +11,9 @@ genex_angular_similarity <- c('../../data/round2/kernels/angular_similarity_gene
                                   '../../data/round2/kernels/angular_similarity_genex_filtered',NA)
 
 genex_dot_product <- c('../../data/round2/kernels/dot_product_genex_original',
-                           '../../data/round2/kernels/dot_product_genex_original',NA)
+                           '../../data/round2/kernels/dot_product_genex_filtered',NA)
 
-cnv_angular_similarity <- c('../../data/round2/kernels/angular_similarity_mutations_driver',NA,NA)
+cnv_angular_similarity <- c('../../data/round2/kernels/angular_similarity_cnv',NA,NA)
 
 cnv_dot_product <- c('../../data/round2/kernels/dot_product_cnv',NA,NA)
 methylation_angular_similarity <- c('../../data/round2/kernels/angular_similarity_methylation_shores_filtered',
@@ -37,4 +37,4 @@ names(kernel_names) <- c("mutations_angular_similarity","mutations_dot_product",
                          ,"genex_dot_product","cnv_angular_similarity","cnv_dot_product","methylation_angular_similarity"
                          ,"methylation_dot_product","drug_angular_similarity","drug_dot_product")
 
-write.table(kernel_names,"kernel_names.txt",sep="\t",row.names = F,col.names = T,quote = F)
+write.table(kernel_names,"../../data/round2/kernels/kernel_names.txt",sep="\t",row.names = F,col.names = T,quote = F)
